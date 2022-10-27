7ac5aecfe506d2bb61ebfbeb6e6efdf9088fbcb7
sarkerjahiduzzaman <78559065+sarkerjahiduzzaman@users.noreply.github.com>
2022-10-27 16:37:33 +0600

    not is added

---
+++ src/page/Not/Not.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Not = () => {
+    return (
+        <div>
+            <div >
+                <h1> 404 Page Not found</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Not;
+
